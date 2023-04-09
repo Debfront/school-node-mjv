48cb052773e402a2c227fad9ba047417174da6e9
Débora Monteiro <deborah_monteiro12@hotmail.com>
2023-04-09 17:50:33 -0300

    crud-banco-de-dados

--- projeto-school-node/src/routers/students.router.ts
+++ projeto-school-node/src/routers/students.router.ts
@@ -3,46 +3,44 @@ import StudentsService from "../services/students.service";
 
 const router = Router();
 
-
-
-router.get('/', (req: Request, res: Response) => {
-    const  students = StudentsService.getAll();
+router.get('/', async (req: Request, res: Response) => {
+    const students = await StudentsService.getAll();
     res.send(students);
 });
 
-router.get('/:document', (req: Request, res: Response) => {
-    const student = StudentsService.getByDocument(req.params.document);
-    if (!student) return res.status(400).send({ message: "Estudante não encontrado!"});
+router.get('/:document', async (req: Request, res: Response) => {
+    const student = await StudentsService.getByDocument(req.params.document);
+    if (!student) return res.status(400).send({ message: "Estudante não encontrado!" });
     res.status(200).send(student);
 });
 
-router.post('/', (req: Request, res: Response) => {
-    if (req.body.age < 18){
-        return res.status(400).send({ message: 'Estudante não criado pois não tem a idade mínina(18 anos).'});
+router.post('/', async (req: Request, res: Response) => {
+    if (req.body.age < 18) {
+        return res.status(400).send({ message: 'Estudante não criado pois não tem a idade mínina(18 anos).' });
     }
-    StudentsService.create(req.body);
-    res.status(201).send({message: 'Estudante Criado com Sucesso!'});
+    await StudentsService.create(req.body);
+    res.status(201).send({ message: 'Estudante Criado com Sucesso!' });
 });
 
 
-router.delete('/remove/:document', (req: Request, res: Response) => {
-     try {
-        StudentsService.remove(req.params.document);
-        res.status(200).send({ message: " Estudante removido com sucesso! "});
-     } catch(error: any ){
-        res.status(400).send({ message: error.message});
-     }
-   
-});
-
-router.put('/:document', (req: Request, res: Response) => {
+router.delete('/remove/:document', async (req: Request, res: Response) => {
     try {
-        StudentsService.update(req.params.document, req.body);
-        res.status(200).send({ message: "Estudante atualizado com sucesso!"});
-    }catch(error: any){
-        res.status(400).send({ message: error.message});
+        await StudentsService.remove(req.params.document);
+        res.status(200).send({ message: " Estudante removido com sucesso! " });
+    } catch (error: any) {
+        res.status(400).send({ message: error.message });
     }
-    
+
+});
+
+router.put('/:document', async (req: Request, res: Response) => {
+    try {
+        await StudentsService.update(req.params.document, req.body);
+        res.status(200).send({ message: "Estudante atualizado com sucesso!" });
+    } catch (error: any) {
+        res.status(400).send({ message: error.message });
+    }
+
 });
 
 export default router;
--- projeto-school-node/src/services/students.service.ts
+++ projeto-school-node/src/services/students.service.ts
@@ -1,68 +1,26 @@
+import StudentRepository from "../repositories/student.repository";
 import { Student } from "../models/student.model";
 
-
 class StudentsService {
-    students: Array<Student> = [
     
-        {
-            name: 'Nathan Carlos',
-            email: 'nathan@gmail.com',
-            document: '15901586018',
-            password:'21882139046',
-            age: 22,
-            phone: '129976543'
-        },
-        {
-            name: 'Gabriel Silva',
-            email: 'gabriel@gmail.com',
-            document: '98994257020',
-            password:'54321',
-            age: 25
-        },
-        {
-            name: 'Palloma Cristina',
-            email: 'palloma@gmail.com',
-            document: '94251331079',
-            password:'1233456',
-            age: 21
-        },
-        {
-            name: 'Maria Souza',
-            email: 'maria@gmail.com',
-            document: '60779412044',
-            password:'1233456',
-            age: 27
-        },
-    
-   ];
-
    getAll () {
-      return this.students;
+      return StudentRepository.getAll();
    }
 
    getByDocument(document: string) {
-       const student =  this.students.find((std) => std.document === document);
-       return student;
+      return StudentRepository.getByDocument(document);
    }
 
-   create(student: Student){
-        this.students.push(student);
+   create(student: typeof Student){
+        return StudentRepository.create(student);
    }
 
    remove(document: string) {
-    const studentIndex = this.students.findIndex((student) => student.document === document);
-    if (studentIndex === -1){
-        throw new Error( " Estudante não encontrado!");
-       }
-    this.students.splice(studentIndex, 1);
+        return StudentRepository.remove(document);
    }
 
-   update(document: string, student: Student){
-    const studentIndex = this.students.findIndex((student) => student.document === document);
-    if (studentIndex === -1){
-       throw new Error("Estudante não encontrado");
-    }
-   this.students[studentIndex] = student;
+   update(document: string, student: Partial<typeof Student>){
+        return StudentRepository.update(document, student);
    }
 }
 
